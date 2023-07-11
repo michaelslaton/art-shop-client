a51cf51f298d88eb6b56d9d8a389ac6639417f0a
michaelslaton <michaelaaronphoto@gmail.com>
2023-07-11 12:33:21 -0700

    populated dummy data, added react-app.d.ts file

---
+++ src/data/dummy-data.ts
@@ -0,0 +1,141 @@
+import art1 from '../assets/imgs/product (1).png';
+import art2 from '../assets/imgs/product (2).png';
+import art3 from '../assets/imgs/product (3).png';
+import art4 from '../assets/imgs/product (4).png';
+import art5 from '../assets/imgs/product (5).png';
+import art6 from '../assets/imgs/product (6).png';
+import art7 from '../assets/imgs/product (7).png';
+import art8 from '../assets/imgs/product (8).png';
+import art9 from '../assets/imgs/product (9).png';
+import art10 from '../assets/imgs/product (10).png';
+import art11 from '../assets/imgs/product (11).png';
+import art12 from '../assets/imgs/product (12).png';
+import art13 from '../assets/imgs/product (13).png';
+import art14 from '../assets/imgs/product (14).png';
+import art15 from '../assets/imgs/product (15).png';
+import art16 from '../assets/imgs/product (16).png';
+import art17 from '../assets/imgs/product (17).png';
+
+const dummyData = [
+  {
+    id: 0,
+    title: "Fae City",
+    description: "A Fae City.",
+    img: art1,
+    tags: ["fantasy", "city"],
+  },
+  {
+    id: 1,
+    title: "Strange Cat",
+    description: "A Strange Cat.",
+    img: art2,
+    tags: ["fantasy", "animal", "cat"]
+  },
+  {
+    id: 2,
+    title: "Noble Cat",
+    description: "A Noble Cat.",
+    img: art3,
+    tags: ["fantasy", "animal", "cat"]
+  },
+  {
+    id: 3,
+    title: "A Cat",
+    description: "A Cat.",
+    img: art4,
+    tags: ["animal", "cat", "surreal"]
+  },
+  {
+    id: 4,
+    title: "An Orange Cat",
+    description: "An orange cat in a Fae forest.",
+    img: art5,
+    tags: ["fantasy", "animal", "cat"]
+  },
+  {
+    id: 5,
+    title: "An Cat in Space",
+    description: "A cat in space.",
+    img: art6,
+    tags: ["sci-fi", "animal", "cat", "surreal"]
+  },
+  {
+    id: 6,
+    title: "Alien Lizard",
+    description: "An alien lizard on a strange planet.",
+    img: art7,
+    tags: ["sci-fi", "animal", "lizard"]
+  },
+  {
+    id: 7,
+    title: "A large eyed Alien Lizard",
+    description: "An alien lizard with large green eyes.",
+    img: art8,
+    tags: ["surreal", "animal", "lizard"]
+  },
+  {
+    id: 8,
+    title: "Small lizard",
+    description: "A small lizard.",
+    img: art9,
+    tags: ["surreal", "animal", "lizard"]
+  },
+  {
+    id: 9,
+    title: "Best lizard",
+    description: "A lizard.",
+    img: art10,
+    tags: ["surreal", "animal", "lizard"]
+  },
+  {
+    id: 10,
+    title: "Alien City",
+    description: "An alien city.",
+    img: art11,
+    tags: ["sci-fi", "city"]
+  },
+  {
+    id: 11,
+    title: "Alien World",
+    description: "An alien city on an alien world.",
+    img: art12,
+    tags: ["sci-fi", "city"]
+  },
+  {
+    id: 12,
+    title: "Alien Metropolis",
+    description: "An alien metropolis.",
+    img: art13,
+    tags: ["sci-fi", "city"]
+  },
+  {
+    id: 13,
+    title: "Alien Holy City",
+    description: "An alien holy city.",
+    img: art14,
+    tags: ["sci-fi", "city"]
+  },
+  {
+    id: 14,
+    title: "Alien cityscape",
+    description: "An alien cityscape on an alien world.",
+    img: art15,
+    tags: ["sci-fi", "city"]
+  },
+  {
+    id: 15,
+    title: "A snowflake",
+    description: "A surreal, macro snowflake.",
+    img: art16,
+    tags: ["surreal", "snowflake", "macro"]
+  },
+  {
+    id: 16,
+    title: "Snowflake on fire",
+    description: "A blazing snowflake.",
+    img: art17,
+    tags: ["surreal", "snowflake", "macro"]
+  },
+]
+
+export default dummyData;
