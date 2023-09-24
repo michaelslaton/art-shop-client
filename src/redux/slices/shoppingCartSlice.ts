930ff09c0c4dd47e5bee93634393cf2d2c7d8e51
RenShadecrest <michaelaaronphoto@gmail.com>
2023-09-23 21:05:19 -0700

    converted CartPRoduct from interface to type

--- src/redux/slices/shoppingCartSlice.ts
+++ src/redux/slices/shoppingCartSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import Product from "../../types/Product";
 
-export interface CartProduct extends Product {
+export type CartProduct = Product & {
   quantity: number;
 };
 
