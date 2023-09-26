1ea8bddabb03c1c132fa01590de536a95841dffb
RenShadecrest <michaelaaronphoto@gmail.com>
2023-09-25 20:10:48 -0700

    Upgraded the React Router to version 6.16

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./Layout/home-page/HomePage";
+import Categories from "./Layout/categories/Categories";
+import ShoppingCart from "./Layout/shopping-cart/ShoppingCart";
+import Error404 from "./utils/error404/Error404";
 import Layout from "./Layout/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>,
+      },
+      {
+        path: "/category/:category",
+        element: <Categories/>,
+      },
+      {
+        path: "/shoppingcart",
+        element: <ShoppingCart/>
+      },
+      {
+        path: "*",
+        element: <Error404/>,
+      }
+    ],
+  },
+]);
 
 const App: React.FC = () => {
   return (
     <>
-      <Layout/>
+      <RouterProvider router={router}/>
     </>
   );
 };
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,33 +1,19 @@
 import { useAppSelector } from "../redux/hooks";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-import HomePage from "./home-page/HomePage";
-import Categories from "./categories/Categories";
-import ShoppingCart from "./shopping-cart/ShoppingCart";
-import Error404 from "../utils/error404/Error404";
 import "./layout.css";
+
+
 
 const Layout: React.FC = () => {
   const lightsOn: boolean = useAppSelector((state)=>state.lights.lightsOn);
 
   return (
-    <div className={`main-screen ${lightsOn ? "" : "dark"}`}>
-      <div>
-        <Router>
-          <Header/>
-          <Routes>
-            
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/category/:category" element={<Categories/>}/>
-            <Route path="/shoppingcart" element={<ShoppingCart/>}/>
-
-            <Route path="*" element={<Error404/>}/>
-            
-          </Routes>
-          <Footer/>
-        </Router>
-      </div>
+    <div className={`main-screen ${ lightsOn ? "" : "dark" }`}>
+      <Header/>
+      <Outlet/>
+      <Footer/>
     </div>
   );
 };
